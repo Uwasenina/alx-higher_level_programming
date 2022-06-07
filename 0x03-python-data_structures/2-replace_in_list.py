8f1ac81cb161af8701d3f74255502f3345c2043e
uwasenina <uwasenina63@gmail.com>
2022-06-06 19:59:28 -0700

    Replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(,y_list, idx, element):
+def replace_in_list(my_list, idx, element):
     if idx > my_list - 1 or idx < 0:
         return my_list
     my_list[idx] = element
