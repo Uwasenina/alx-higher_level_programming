140ad9e9f8399a278b44b9d4441a824c27e47b2c
uwasenina <uwasenina63@gmail.com>
2022-06-08 00:20:58 -0700

    Hidden chmod

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
-        if name.startwith('__'):
+        if name.startswith('__'):
             continue
         print("{:s}".format(name))
